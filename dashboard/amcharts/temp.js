5c6bb0d87f9ef43e8e813207c1b24482684f2b4b
clararubin <clararubin@nyu.edu>
2016-11-23 12:18:29 -0500

    updated view w hardcoded data

--- dashboard/amcharts/temp.js
+++ dashboard/amcharts/temp.js
@@ -1,8 +1,8 @@
-var dataset;
-var firstDate = new Date();
-firstDate.setHours( 0, 0, 0, 0 );
-firstDate.setDate( firstDate.getDate() - 10 );
 
+
+/**
+ * buttons that user can click on to display data over particular duration
+ */
 var periods = 
   [ 
     {
@@ -35,37 +35,8 @@ var periods =
     } 
   ]
 
-//     
 
-// TEMPORARY RANDOM DATA GENERATOR
-function generateChartData( data, field ) {
-  var ret = [];
-  for (var i = 0; i < 10; i++) {
-    
-    var randVal = Math.floor(Math.random() * (100));
-    ret.push({
-      "value": randVal,
-    });
-  }
 
-  function compare(a,b) {
-    if (a.value < b.value)
-      return -1;
-    if (a.value > b.value)
-      return 1;
-    return 0;
-  }
-
-  ret.sort(compare);
-
-  for (var i = 0; i < ret.length; i++) {
-    var newDate = new Date(firstDate);
-    newDate.setDate(newDate.getDate() + i );
-    ret[i]["date"] = newDate;
-  }
-
-  return ret;
-}
 
 
 
@@ -118,24 +89,65 @@ var chart = AmCharts.makeChart( "chartdiv", {
 
 
 
-jQuery(document).ready(function() {
-    // REMOVE DATASET
-    jQuery( ".btn-dataset-remove" ).on( "click", function() {
-    if ( chart.dataSets.length > 1 ) {
-      // REMOVE LATEST DATASET AND VALIDATE
-      var dataset = chart.dataSets.pop();
-      chart.validateNow();
-
-    }
-    } );
 
 
-    // ADD DATASET
-    jQuery( ".btn-dataset-add" ).on( "click", function() {
+
+/**
+ * GET RANDOM DATA AND ADD TO DASHBOARD
+ */
 
 
+var numberOfDataSets = 30;
+window.onload= function(){
+  for (var i = 0; i < numberOfDataSets; i++) {
+    populateData();
+  }
+} 
+
+
+var startDate = new Date();
+startDate.setHours( 0, 0, 0, 0 );
+startDate.setDate( startDate.getDate() - 10 );
+
+
+// jQuery( ".btn-dataset-add" ).on( "click", function() {
+
+
+
+// TEMPORARY RANDOM DATA GENERATOR
+function generateChartData( data, field ) {
+  var ret = [];
+  for (var i = 0; i < 10; i++) {
+    
+    var randVal = Math.floor(Math.random() * (12));
+    ret.push({
+      "value": randVal,
+    });
+  }
+
+  function compare(a,b) {
+    if (a.value < b.value)
+      return -1;
+    if (a.value > b.value)
+      return 1;
+    return 0;
+  }
+
+  ret.sort(compare);
+
+  for (var i = 0; i < ret.length; i++) {
+    var newDate = new Date(startDate);
+    newDate.setDate(newDate.getDate() + i );
+    ret[i]["date"] = newDate;
+  }
+
+  return ret;
+}
+
+
+function populateData(){
       // CREATE DATASET
-      dataset = new AmCharts.DataSet();
+      var dataset = new AmCharts.DataSet();
       dataset.title = "Dataset " + ( chart.dataSets.length + 1 );
       dataset.dataProvider = generateChartData();
       dataset.categoryField = "date";
@@ -161,6 +173,4 @@ jQuery(document).ready(function() {
       // ADD NEW DATASET
       chart.dataSets.push( dataset );
       chart.validateNow();
-    } );
-
-} );
+    } 
