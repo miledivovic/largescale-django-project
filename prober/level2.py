ce34326b46153737502dab8e5d16b595bddc1c71
Amritanshu Kajaria <kajaria.amritanshu@gmail.com>
2016-12-15 16:47:30 -0500

    prober

--- prober/level2.py
+++ prober/level2.py
@@ -73,7 +73,7 @@ def prober ():
                     last_error = datetime.datetime.now()
                 
 
-                sqlTwo = "UPDATE dashboard_node SET last_probed = \'" + str(curr_time) + "\', last_status = '" + str(node_status) + "', last_failure = \'" + str(last_error) + "\', error_msg = \'" + node_error + "\' WHERE node_id = " + str(node_id)
+                sqlTwo = "UPDATE dashboard_node SET last_probed = \'" + str(curr_time) + "\', last_status = '" + str(node_status) + "', last_failure = \'" + str(curr_time) + "\', error_msg = \'" + node_error + "\' WHERE node_id = " + str(node_id)
 
                 print (sqlTwo)
                 try:
@@ -83,6 +83,7 @@ def prober ():
                     conn.commit()
                 except:
                     # Rollback in case there is any error
+                    print("I AM FAILING")
                     conn.rollback()
 
             if first_value == False:
