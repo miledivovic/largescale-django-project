b0e9d149a195ba114efb36c78b5addcece7a27dc
Philip Yoo <philipsdyoo@msn.com>
2016-12-15 17:12:09 -0500

    Demo changes

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -93,7 +93,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'helloworld.urls'
+ROOT_URLCONF = 'demo.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
